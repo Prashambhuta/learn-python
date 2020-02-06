48bace7b88fe62e5e5826e35b7c8a746d6137a7e
Prashambhuta <prasham.bhuta@gmail.com>
2020-02-06 22:52:03 +0530

    First exercise in the form of a dice simulator, major loops used: while, if, else

---
+++ my-exercises/dice-simulator/README.md
@@ -0,0 +1,6 @@
+### Dice Simulator
+To develop a program that simulates a cubic dice.
+
+____
+
+* Single Dice
---
+++ my-exercises/dice-simulator/single-dice.py
@@ -0,0 +1,21 @@
+# Single 6 faced dice
+import random
+print("Welcome to Dice Simulator. Press 'Enter' to start.")
+Enter = input()
+while Enter == "":
+    x = random.randint(1,6)
+    print(x)
+    if x == 6:
+        print ("Roll Again. Press 'Enter'")
+        Enter = input()
+        if Enter == "":
+            x = random.randint(1,6)
+            print(x)
+            if x == 6:
+                print("Two chances only.")
+    print("Next Player Turn. Press 'Enter'")
+    Enter = input()
+else:
+    print("You did not press Enter")
+
+
