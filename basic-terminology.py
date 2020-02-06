f4a58a46f221377473e65c8c2fe1ab2933a7106b
Prashambhuta <prasham.bhuta@gmail.com>
2020-02-06 22:49:08 +0530

    basic terminology for python

---
+++ basic-terminology.py
@@ -0,0 +1,12 @@
+def hello(name):
+    print("Hello %s" % name)
+
+hello("Prasham")
+hello("Abhu")
+
+# Argument = value passed in function call/calling the function
+# Parameter = variable inside the function, mostly when defining the function.
+
+# In this example
+# Argument = 'Prasham', 'Abhu'
+# Parameter = name
