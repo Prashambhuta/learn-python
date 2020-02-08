aec8894153e77de4fd01d9edd7660a79e695d0ad
Prashambhuta <prasham.bhuta@gmail.com>
2020-02-08 16:18:36 +0530

    minor changes

--- my-exercises/madlibs/madlibs1.py
+++ my-exercises/madlibs/madlibs1.py
@@ -1,7 +1,4 @@
 # Madlibs Generator
-# There is no pain you are receding, A distant ship smoke on the horizon,
-# You are only coming through in waves, Your lips move but I can't hear what you are saying,
-# For I have become comfortably numb.
 
 noun = []
 adjective = []
@@ -9,6 +6,8 @@ verb = []
 adverb = []
 place = []
 bodypart = []
+
+print("Welcome to Madlib Generator by Prasham.")
 
 noun1 = str(input("Enter Noun:"))
 verb2 = str(input("Enter Verb ending with -ing:"))
@@ -23,7 +22,7 @@ noun.extend((noun1, noun2))
 bodypart.append((bodypart1))
 adverb.append((adverb1))
 adjective.append((adjective1))
-print(noun)
 
+print("Aha, here is the fun madlib. \n")
 print("There is no %s you are %s. A distant %s smokes on the horizon. You are only %s through in waves. \n"
       "Your %s moves but I can't hear what your saying. You have become %s %s." % (noun[0], verb[0], noun[1], verb[1], bodypart[0], adverb[0], adjective[0]))
