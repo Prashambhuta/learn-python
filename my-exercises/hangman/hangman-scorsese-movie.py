63e350ff8c108ba40e1d7263ca5dff8d54fd830f
Prashambhuta <prasham.bhuta@gmail.com>
2020-02-27 20:41:10 +0530

    Finished complete hangman game based on martin scorsese movies.

--- my-exercises/hangman/hangman-scorsese-movie.py
+++ my-exercises/hangman/hangman-scorsese-movie.py
@@ -1,5 +1,5 @@
 """
-Hangman game based on filmography of Martin Scorcese.
+Hangman game based on filmography of Martin Scorsese.
 """
 import random
 import re
@@ -66,69 +66,88 @@ HangmanPics = ['''
  / \  |
    ======''']
 
-# Convert Movies to lower string
-moviesLower = [n.lower() for n in Movies]
-
-
-# Generate a random movie using random
-movieIndex = random.randint(0, len(moviesLower))
-ranMovie = moviesLower[movieIndex - 1]
-iranMovie = list(ranMovie)
-
-# Convert alphabets into * and display movie to user.
-dispMovie = "".join(
-    ["-" if x.isalpha() else x for x in ranMovie])
-print('''Welcome to hangman game.
-%s
-
-Here is the movie: 
-
-%s ''' % (HangmanPics[0], dispMovie))
-
-# From here user interaction begins.
+# Variables defined
 total_guess = []
 MAXIMUM_TRY = 6
 correct_guess = []
 incorrect_guess = []
 
+# Convert Movies to lower string
+moviesLower = [n.lower() for n in Movies]
 
+# Generate a random movie using random
+movieIndex = random.randint(0, len(moviesLower))
+
+# generate a ranMovie in form of string
+ranMovie = moviesLower[movieIndex - 1]
+
+# convert str into list for easy manipulation
+iranMovie = list(ranMovie)
+
+# Convert alphabets into '-'(str) and display movie to user.
+dispMovie = "".join(
+    ["-" if x.isalpha() and x not in correct_guess else x for
+     x in
+     ranMovie])
+
+print('''Welcome to hangman game.
+%s\nHere is the movie: 
+
+%s ''' % (HangmanPics[0], dispMovie))
+
+# convert dispMovie into list for easy manipulation
+list_dispMovie = list(dispMovie)
+
+
+# Game starts
 def user_interaction():
     while len(incorrect_guess) < MAXIMUM_TRY:
-        user_input = input("\nTake a guess:").lower()
+        user_input = input("\nTake a guess:\n").lower()
         if len(user_input) > 1:
-            print("\nInvalid Input!, \nGuess 1 letter at a time.")
+            print("\nMultiple Enteries!, \nInput 1 letter at a time.")
         elif len(user_input) == 0:
             print("\nInvalid Input! \nYou pressed enter")
         elif user_input == ' ' and len(user_input) == 1:
             print("\nInvalid Input! You pressed space")
-        elif user_input in total_guess:
+        elif user_input in incorrect_guess:
             print("\nInavlid Input! \nAlready guessed")
-        # elif user_input in incorrect_guess:
-        #     print("Already guessed.")
+        elif user_input in correct_guess:
+            print("\nCorrectly guessed before.")
+        elif user_input.isnumeric():
+            print("\nInavlid Input! \nNumber entered")
         elif len(user_input) == 1 and user_input != ' ' and \
                 user_input.isalpha() and user_input in iranMovie and \
                 user_input not in correct_guess:
             total_guess.append(user_input)
             correct_guess.append(user_input)
-            print(correct_guess)
-            print(range(len(correct_guess)))
-
-            # TODO: save & display correct guesses in dispMovie
-
+            print("\n CORRECT GUESS:%s \n" % correct_guess)
+            #print(ranMovie.index(user_input))
+            dispMovie = "".join(["-" if x.isalpha() and x not in correct_guess
+                                 else x for x in ranMovie])
+            if dispMovie != ranMovie:
+                pass
+            else:
+                print("SUCCESSFULLY SOLVED")
+                break
         else:
             incorrect_guess.append(user_input)
             total_guess.append((user_input))
-            print("Incorrect guess, I will now show the next step in hangman")
+            print("\nINCORRECT !")
+            print(HangmanPics[len(incorrect_guess)])
+            print("\n CORRECT GUESS:%s \n" % correct_guess)
+        if len(correct_guess) != 0:
+            print(dispMovie)
+    print('''GAME OVER!!!, 
+Your incorrect guess were:
+%s\n''' % incorrect_guess)
+    print('''The correct answer was: \n 
+%s\n''' % ranMovie.upper())
 
-    print('''Tries finished, your guess were:
-    %s''' % incorrect_guess)
-    print('''The correct answer was:
-    %s''' % ranMovie)
-    print(total_guess)
-    print(correct_guess)
+    print('''Complete guess were: 
+%s''' % total_guess)
 
 
-
-
-while len(incorrect_guess) < MAXIMUM_TRY:
+if dispMovie == ranMovie:
+    print("FINISH")
+elif len(incorrect_guess) < MAXIMUM_TRY:
     user_interaction()
