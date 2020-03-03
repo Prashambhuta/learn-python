e35a3863d049e2299794209a85a46c6d18f98a63
Prashambhuta <prasham.bhuta@gmail.com>
2020-03-03 19:39:59 +0530

    My solution to the problem.

--- edx-mitx-6.00.1x/pset1_p3.py
+++ edx-mitx-6.00.1x/pset1_p3.py
@@ -1,33 +1,48 @@
 """
-# TODO: Finding out longest sub strings that are in alphabetical order.
+Using simple iteration
 """
-s = "sdsfdabc"
-result = []
-temp = []
+s = "zysdaeqskadopjfabcdefgkjhajkfhxcb"
+result = ''
+temp = ''
 iteration = len(s)
+
+# new code - 02 March
+
+for iteration in range(len(s)):
+    temp += s[iteration]
+    if len(temp) > len(result):
+        result = temp
+    if iteration > len(s) - 2:
+        break
+    if s[iteration] > (s[iteration + 1]):
+        temp = ''
+print(result)
+
+
+
 
 # generate all subsets of string
 
-while iteration != 0:
-    for i in range(0, iteration):
-        temp.append(s[i:iteration])
-    iteration -= 1
-
-print(temp)
-
-
-
-# TODO: remove elements from list that are not of alphabetical order.
-i = ''
-for strings in temp:
-        #print(strings)
-        for n in range(len(strings)):
-            if strings[n:n+1] >= strings[n-1:n]:
-                i += strings[n:n+1]
-for z in range(len(i)):
-    if i[z:z+1] >= i[z-1:z]:
-        print(i[z:z+1])
-print(i)
+# while iteration != 0:
+#     for i in range(0, iteration):
+#         temp.append(s[i:iteration])
+#     iteration -= 1
+#
+# print(temp)
+#
+#
+#
+# # remove elements from list that are not of alphabetical order.
+# i = ''
+# for strings in temp:
+#         #print(strings)
+#         for n in range(len(strings)):
+#             if strings[n:n+1] >= strings[n-1:n]:
+#                 i += strings[n:n+1]
+# for z in range(len(i)):
+#     if i[z:z+1] >= i[z-1:z]:
+#         print(i[z:z+1])
+# print(i)
 
 
 # for strings in temp:
