74ad89c29dc42fb5fb6da623a68bd5f7b244cfd7
Prashambhuta <prasham.bhuta@gmail.com>
2020-03-03 19:41:04 +0530

    Understanding debugging in pyCharm.

---
+++ udemy-atbs/37_b_using_debugger.py
@@ -0,0 +1,15 @@
+"""
+Using debugger on simple simulation of coin toss.
+"""
+
+import random
+
+heads = 0
+total = 0
+for i in range(0,1000):
+    number = random.randint(0,1)
+    if number == 1:
+        heads += 1
+    total += 1
+print(heads)
+print(total)
