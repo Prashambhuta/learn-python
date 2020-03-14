23775e7cf89f92cb0038350b634a16b8376450be
Prasham Bhuta <prasham.bhuta@gmail.com>
2020-03-14 19:38:05 +0530

    cosmetic change

--- udemy-atbs/39_request_module.py
+++ udemy-atbs/39_request_module.py
@@ -1,3 +1,4 @@
+#! usr/bin/env python3
 import requests
 res = requests.get('http://automatetheboringstuff.com/files/rj.txt')    #
 # returns a Response Object
@@ -8,7 +9,7 @@ print(res.raise_for_status())   # Check status of 'res', returns None if
 ROMEO_JULIET = open('39_romeo_and_juliet.txt', 'wb')   # Append 'res' to a
 # local file. Use 'wb' to write in binary.
 
-for chunks in res.iter_content(10000): # Write binary in form of chunks.
+for chunks in res.iter_content(10000):  # Write binary in form of chunks.
     # Each chunk of 10000 unicode will be iterated and appended to the file
     # ROMEO_JULIET
     ROMEO_JULIET.write(chunks)
