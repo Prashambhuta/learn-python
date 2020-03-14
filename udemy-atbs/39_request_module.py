c35943c0fdec72e896519ec63493c33907de3b05
Prashambhuta <prasham.bhuta@gmail.com>
2020-03-14 19:31:59 +0530

    ATBS: Chapter 39: Using the request module.

---
+++ udemy-atbs/39_request_module.py
@@ -0,0 +1,16 @@
+import requests
+res = requests.get('http://automatetheboringstuff.com/files/rj.txt')    #
+# returns a Response Object
+
+print(res.raise_for_status())   # Check status of 'res', returns None if
+# successful
+
+ROMEO_JULIET = open('39_romeo_and_juliet.txt', 'wb')   # Append 'res' to a
+# local file. Use 'wb' to write in binary.
+
+for chunks in res.iter_content(10000): # Write binary in form of chunks.
+    # Each chunk of 10000 unicode will be iterated and appended to the file
+    # ROMEO_JULIET
+    ROMEO_JULIET.write(chunks)
+
+ROMEO_JULIET.close()
