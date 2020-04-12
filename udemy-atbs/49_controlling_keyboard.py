278df0c94f607e6aeb60ebdde32d1135f84bbdde
Prashambhuta <prasham.bhuta@gmail.com>
2020-04-12 22:26:29 +0530

    Lesson 49 | Controlling keyboard using pyautogui

---
+++ udemy-atbs/49_controlling_keyboard.py
@@ -0,0 +1,21 @@
+#! usr/bin/env python3
+"""
+Controlling the keyboard using pyautogui
+"""
+
+import pyautogui
+
+# to type text use `typewrite()` method
+pyautogui.typewrite("Hello world!", interval=0.01)
+
+# to press special keys
+pyautogui.typewrite(['d', 'backspace', 'p'], interval=0.1)
+
+# to see all keyboard keys option use `KEYBOARD_KEYS` method
+# print(pyautogui.KEYBOARD_KEYS)
+
+# to press the single key
+# pyautogui.press('f1')
+
+# to press shortcuts or multiple key together
+pyautogui.hotkey('shift', 'f10')
